c773d433b3b7ae8cd5a9e24bbfd1a7a51b6e627e
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-09-15 18:34:55 -0700

    made a basic server that sends games data

---
+++ server.js
@@ -0,0 +1,14 @@
+import { initialState } from './data'
+
+const express = require('express')
+
+const port = 5000
+const server = express()
+
+server.get('/gamelist', (req, res) => {
+    res.status(200).json(initialState.games)
+})
+
+server.listen(port, () => {
+    console.log(`server listening on http://${port}`)
+})
