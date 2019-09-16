9c38d067b32bec7f2b090f633209d19622b773e7
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-09-15 19:02:46 -0700

    fixed port listening issue

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const initialState = require('./data')
 
 const express = require('express')
 
-const port = 5000
+const port = process.env.PORT || 5000
 const server = express()
 
 server.get('/gamelist', (req, res) => {
