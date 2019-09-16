b0a1a70dd31fe110cd2636af79d6be7052ed9a3a
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-09-15 20:40:13 -0700

    fixed cors for real

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const initialState = require('./data')
+const cors = require('cors')
 
 const express = require('express')
 
 const port = process.env.PORT || 5000
 const server = express()
+
+server.use(cors())
 
 server.get('/gamelist', (req, res) => {
     res.status(200).json(initialState.games)
