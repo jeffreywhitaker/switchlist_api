fc3e02473de5d098ab1f11e19d75dc782d42de2d
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-09-23 02:07:03 -0700

    added better logic to user signup and login calls

--- data.js
+++ data.js
@@ -1,90 +1,91 @@
-const state = {
-    games: [
-        {
-            name: 'Super Mario Odyssey',
-            releaseDate: '10-27-2017',
-            series: 'Super Mario',
-            publisher: 'Nintendo',
-            developer: 'Nintendo EPD',
-            directors: [
-                'Kenta Motokura'
-            ],
-            composers: [
-                'Naoto Kubo',
-                'Shiho Fujii',
-                'Koji Kondo',
-            ],
-            caseImg: 'https://upload.wikimedia.org/wikipedia/en/8/8d/Super_Mario_Odyssey.jpg',
-            hdRumble: true,
-            multiplayer: true,
-            cloudSaves: true,
-            genres: [
-                'platform',
-                'action-adventure'
-            ]
-        },
-        {
-            name: 'Dead Cells',
-            releaseDate: '08-07-2018',
-            series: null,
-            publisher: 'Motion Twin',
-            developer: 'Motion Twin',
-            directors: [],
-            composers: [
-                'Yoann Laulan',
-            ],
-            caseImg: 'https://www.mobygames.com/images/covers/l/498109-dead-cells-xbox-one-front-cover.jpg',
-            hdRumble: false,
-            multiplayer: false,
-            couldSaves: true,
-            genres: [
-                'roguelike',
-                'metroidvania'
-            ]
-        },
-        {
-            name: 'Fire Emblem: Three Houses',
-            releaseDate: '06-26-2019',
-            series: 'Fire Emblem',
-            publisher: 'Nintendo',
-            developer: 'Intelligent Systems',
-            directors: [
-                'Toshiyuki Kusakihara',
-                'Genki Yokota',
-            ],
-            composers: [
-                'Takeru Kanazaki',
-                'Hiroki Morishita',
-                'Rei Kondoh',
-            ],
-            caseImg: 'https://upload.wikimedia.org/wikipedia/en/1/16/Fire_Emblem_Three_Houses.jpg',
-            hdRumble: true,
-            multiplayer: false,
-            cloudSaves: true,
-            genres: [
-                'tactical role-playing'
-            ]
-        },
-    ],
-    publishers: [
-        'Nintendo',
-        'Motion Twin',
-        'Activision',
-        'Random Publisher',
-    ]
-}
+const games = [
+    {
+        name: 'Super Mario Odyssey',
+        releaseDate: '10-27-2017',
+        series: 'Super Mario',
+        publisher: 'Nintendo',
+        developer: 'Nintendo EPD',
+        directors: [
+            'Kenta Motokura'
+        ],
+        composers: [
+            'Naoto Kubo',
+            'Shiho Fujii',
+            'Koji Kondo',
+        ],
+        caseImg: 'https://upload.wikimedia.org/wikipedia/en/8/8d/Super_Mario_Odyssey.jpg',
+        hdRumble: true,
+        multiplayer: true,
+        cloudSaves: true,
+        genres: [
+            'platform',
+            'action-adventure'
+        ]
+    },
+    {
+        name: 'Dead Cells',
+        releaseDate: '08-07-2018',
+        series: null,
+        publisher: 'Motion Twin',
+        developer: 'Motion Twin',
+        directors: [],
+        composers: [
+            'Yoann Laulan',
+        ],
+        caseImg: 'https://www.mobygames.com/images/covers/l/498109-dead-cells-xbox-one-front-cover.jpg',
+        hdRumble: false,
+        multiplayer: false,
+        couldSaves: true,
+        genres: [
+            'roguelike',
+            'metroidvania'
+        ]
+    },
+    {
+        name: 'Fire Emblem: Three Houses',
+        releaseDate: '06-26-2019',
+        series: 'Fire Emblem',
+        publisher: 'Nintendo',
+        developer: 'Intelligent Systems',
+        directors: [
+            'Toshiyuki Kusakihara',
+            'Genki Yokota',
+        ],
+        composers: [
+            'Takeru Kanazaki',
+            'Hiroki Morishita',
+            'Rei Kondoh',
+        ],
+        caseImg: 'https://upload.wikimedia.org/wikipedia/en/1/16/Fire_Emblem_Three_Houses.jpg',
+        hdRumble: true,
+        multiplayer: false,
+        cloudSaves: true,
+        genres: [
+            'tactical role-playing'
+        ]
+    },
+]
+
+const publishers = [
+    'Nintendo',
+    'Motion Twin',
+    'Activision',
+    'Random Publisher',
+]
 
 const users = [
     {
         id: 1,
         name: 'Jeff',
         password: 'jeff',
+        email: 'jeff@jeff.com'
     },
     {
         id: 2,
         name: 'Katherine',
-        password: 'mimi'
+        password: 'mimi',
+        email: ''
     }
 ]
 
-module.exports = { state, users }
+module.exports = { games, users, publishers }
--- server.js
+++ server.js
@@ -1,97 +1,129 @@
-const data = require('./data')
-const cors = require('cors')
-const express = require('express')
-const bodyParser = require('body-parser')
+const data = require("./data");
+const cors = require("cors");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const port = process.env.PORT || 5000
-const server = express()
+const port = process.env.PORT || 5000;
+const server = express();
 
-server.use(cors())
-server.use(bodyParser.json())
+server.use(cors());
+server.use(bodyParser.json());
 
-let gameId = data.state.games.length
-let userId = data.users.length
+let gameId = data.games.length;
+let userId = data.users.length;
+let token = "dummytoken";
 
 // authenticator function
 
 function authenticator(req, res, next) {
-    const { authorization } = req.headers
-    if (authorization === token) {
-        next()
-    } else {
-        res.status(403).json({ error: 'You must be logged in to access this section.' })
-    }
+  const { authorization } = req.headers;
+  if (authorization === token) {
+    next();
+  } else {
+    res
+      .status(403)
+      .json({ error: "You must be logged in to access this section." });
+  }
 }
 
-// login API  
+// login API
 
-server.post('/login', (req, res) => {
-    const { username, password } = req.body
-    // map through users and check password
+server.post("/login", (req, res) => {
+  const { username, password, email } = req.body;
 
+  if (!username) {
+    return res.status(400).send("Please provide a username");
+  }
+  const findByUsername = data.users.find(user => {
+    return user.name === username;
+  });
 
-    if (username === 'Jeff' && password === 'jeff') {
-        res.status(200).json({
-            payload: token
-        })
-    } else {
-        res
-            .status(403)
-            .json({ error: 'Username or Password incorrect. Please fix.' })
-    }
-})
+  const findByEmail = data.users.find(user => {
+    return user.email === email;
+  });
+
+  if (!findByUsername && !findByEmail) {
+    return res.status(400).send("Unable to find user in userlist.");
+  }
+
+  const matchingUser = findByUsername || findByEmail;
+
+  if (matchingUser.password === password) {
+    res.status(200).json({
+      token: token
+    });
+  } else {
+    res.status(403).json({ error: "Incorrect password." });
+  }
+});
 
 // games API
 
-server.get('/gamelist', authenticator, (req, res) => {
-    res.status(200).json(data.state.games)
-})
+server.get("/gamelist", (req, res) => {
+  res.status(200).json(data.games);
+});
+
+// get list of publishers
+server.get("/publishers", (req, res) => {
+  res.status(200).json(data.publishers);
+});
 
 // users API
 
-server.post('/users', (req, res) => {
-    if (!req.body.name) {
-        return res.status(400).send("There needs to be a name in the name field.")
-    }
-    if (data.users.find(user => {
-        return user.name === req.body.name
-    })) {
-        return res.status(400).send("There is already a user with this name. Please use another name.")
-    }
-    const newUser = req.body
-    newUser.id = userId
-    data.users.push(newUser)
-    ++userId
-    res.status(201).json(token)
-})
-
-server.put('/users/:id', authenticator, (req, res) => { // add in authorization later
-    if (!req.params.id)
-        return res.status(400).send("Your request is missing the user id.")
-    if (
-        !req.body.name ||
-        !req.body.password
-    ) {
-        return res.status(422).send("Make sure your request body has all the fields it needs.")
-    }
-    data.users = data.users.map(user => {
-        if (user.id === parseInt(req.params.id)) {
-            return req.body
-        }
-        return user
+// signup logic
+server.post("/users", (req, res) => {
+  if (!req.body.username) {
+    return res.status(400).send("There needs to be a name in the name field.");
+  }
+  if (
+    data.users.find(user => {
+      return user.name === req.body.username;
     })
-    res.status(200).send(req.body)
-})
+  ) {
+    return res
+      .status(400)
+      .send("There is already a user with this name. Please use another name.");
+  }
+  if (!req.body.password || req.body.password !== req.body.confirmPassword) {
+    return res.status(400).send("Two matching passwords required.");
+  }
+  const newUser = req.body;
+  newUser.id = userId;
+  data.users.push(newUser);
+  ++userId;
+  res.status(201).json(token);
+});
 
-server.delete('/users/:id', authenticator, (req, res) => {  // add in authorization later
-    if (!req.params.id)
-        res.status(400).send('Your request is missing the user id')
-    data.users = data.users.filter(user => `${user.id}` !== req.params.id)
-    res.status(202).send("User has been deleted.")
-})
+// update user logic
+server.put("/users/:id", authenticator, (req, res) => {
+  // add in authorization later
+  if (!req.params.id)
+    return res.status(400).send("Your request is missing the user id.");
+  if (!req.body.username || !req.body.password) {
+    return res
+      .status(422)
+      .send("Make sure your request body has all the fields it needs.");
+  }
+  data.users = data.users.map(user => {
+    if (user.id === parseInt(req.params.id)) {
+      return req.body;
+    }
+    return user;
+  });
+  res.status(200).send(req.body);
+});
+
+// delete user logic
+server.delete("/users/:id", authenticator, (req, res) => {
+  // add in authorization later
+  if (!req.params.id)
+    res.status(400).send("Your request is missing the user id");
+  data.users = data.users.filter(user => `${user.id}` !== req.params.id);
+  res.status(202).send("User has been deleted.");
+});
 
 // server port
 
 server.listen(port, () => {
-    console.log(`server listening on http://${port}`)
-})
+  console.log(`server listening on http://${port}`);
+});
